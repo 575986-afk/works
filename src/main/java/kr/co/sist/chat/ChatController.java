8e940e9794216b92b2a90789c1c45da8e05796d4
쌍용교육센터 <@DESKTOP-QR0CQTO>
2026-08-20 16:46:53 +0900

    mappers rename

--- src/main/java/kr/co/sist/chat/ChatController.java
+++ src/main/java/kr/co/sist/chat/ChatController.java
@@ -4,11 +4,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpSession;
+import kr.co.sist.signup.UserDTO;
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+	
+	private final ChatService cs;
 
 	@GetMapping("/chatting")
-	public String chatting() {
+	public String chatting(HttpSession session, Model model,String userNo ) {
+		UserDTO loginUser = (UserDTO) session.getAttribute("user");
+	    if (loginUser == null) {
+	        return "redirect:/login"; 
+	    }
+		cs.getChatRoomList(null);
+		
 		return "works/chat/chatting";
 	}
 	
