cb394e2032ac07c16e99d091c9c5e245e511d032
쌍용교육센터 <@DESKTOP-QR0CQTO>
2026-08-21 16:42:01 +0900

    todoDomain fix

--- src/main/java/kr/co/sist/userDashboard/TodoDomain.java
+++ src/main/java/kr/co/sist/userDashboard/TodoDomain.java
@@ -1,6 +1,16 @@
 package kr.co.sist.userDashboard;
 
+import java.security.Timestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class TodoDomain {
 
-	private String todo_no, user_no, title, content, status, start_date, end_date, bookmark;
+	private String todo_no, user_no, title, content, status, bookmark;
+	private Timestamp start_date, end_date;
 }
