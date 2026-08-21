21ebff28126452dd432b8d33d6d502f33b840b76
쌍용교육센터 <@DESKTOP-QR0CQTO>
2026-08-21 16:39:46 +0900

    todoDomain update

--- src/main/java/kr/co/sist/userDashboard/TodoDomain.java
+++ src/main/java/kr/co/sist/userDashboard/TodoDomain.java
@@ -2,5 +2,5 @@ package kr.co.sist.userDashboard;
 
 public class TodoDomain {
 
-	private String todo_No;
+	private String todo_no, user_no, title, content, status, start_date, end_date, bookmark;
 }
