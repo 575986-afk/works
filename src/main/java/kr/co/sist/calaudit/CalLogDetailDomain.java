052e002b6563c6e6657ba4b5a1d7b10e699f3918
user <user@DESKTOP-3EFR9Q6>
2026-08-24 13:49:47 +0900

    캘린더 디테일 경로 수정

--- src/main/java/kr/co/sist/calaudit/CalLogDetailDomain.java
+++ src/main/java/kr/co/sist/calaudit/CalLogDetailDomain.java
@@ -9,17 +9,8 @@ import lombok.ToString;
 @ToString
 public class CalLogDetailDomain {
     private String logNo;
-    private String companyNo;
     private String title;          // 제목
-    private String status;         // 상태
     private String startDate;      // 기한 (시작일)
     private String endDate;        // 기한 (종료일)
     
-    // 요청자 (작성자)
-    private String requesterName;  
-    private String requesterEmail; 
-    
-    // 담당자
-    private String assigneeName;   
-    private String assigneeEmail;  
 }
