a63523727bcf27b8f1642fc5dd4625db5815c948
쌍용교육센터 <@DESKTOP-QR0CQTO>
2026-08-26 16:51:46 +0900

    interceptor 추가

--- src/main/java/kr/co/sist/findPw/FindPwService.java
+++ src/main/java/kr/co/sist/findPw/FindPwService.java
@@ -36,9 +36,10 @@ public class FindPwService {
 		map.put("verificationNo", verificationNo);
 		map.put("userNo", userNo);
 
+		session.setAttribute("userNo", userNo);
 		int result = fm.insertCode(map);
 
-		if (result > 0) {
+		if (result != 0) {
 			System.out.println("======================================");
 			System.out.println(">>> [테스트용] 인증번호: " + verificationNo);
 			System.out.println("======================================");
@@ -47,13 +48,12 @@ public class FindPwService {
 	            message.setTo("day305@naver.com");
 	            message.setFrom("day305@naver.com");
 	            message.setSubject("[WORKS] 비밀번호 찾기 인증번호");
-	            message.setText("인증번호는 [" + verificationNo + "] 입니다. <br>10분 이내에 입력해주세요.");
+	            message.setText("인증번호는 [" + verificationNo + "] 입니다. \n10분 이내에 입력해주세요.");
 	            mailSender.send(message);
 	        } catch (Exception e) {
 	            e.printStackTrace();
 	            return false;
 	        }
-			session.setAttribute("userNo", userNo);
 			return true;
 		}
 
@@ -66,6 +66,6 @@ public class FindPwService {
 
 	public int setNewPw(String userNo, String newPw) {
 		String encodedPassword = passwordEncoder.encode(newPw);
-		return fm.updatePw(userNo, encodedPassword); // 암호화된 비밀번호 전달
+		return fm.updatePw(userNo, encodedPassword); 
 	}
 }
