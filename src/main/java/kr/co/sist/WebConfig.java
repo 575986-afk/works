537acbbd01de8d5029a5aa516a4b8529d50f948a
user <user@DESKTOP-PN0E2P7>
2026-08-26 16:45:36 +0900

    인터셉터 수정

--- src/main/java/kr/co/sist/WebConfig.java
+++ src/main/java/kr/co/sist/WebConfig.java
@@ -42,6 +42,8 @@ public class WebConfig implements WebMvcConfigurer{
                    "/",
                    "/admin/**",
                    "/policy/**",
+                   "/user/notices",
+                   "/notice/notice",
                    "/login",
                    "/works/login/**",
                    "/findId",
